68ff809fb27790b95f6b7ad984fb8811b291cd52
Yoshimasa Sakuragi <ysrhsp@outlook.com>
2017-02-26 00:04:51 +0900

    Transplanted previous shaders

---
+++ Assets/Expreriments/ModelDistortion/ModelDistortionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelDistortionController : MonoBehaviour {
+
+	[SerializeField]
+	private List<Material> mats;
+
+	private int matIndex;
+	[SerializeField]
+	private Material targetMat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetMat.SetFloat ("_Amount", Mathf.Abs( Mathf.Cos(Time.fixedTime * 1.5f)) * 0.05f);
+		transform.Rotate (new Vector3(1.0f, 1.0f, 1.0f));
+	}
+}
